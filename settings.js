689a7b7006c75a39a8c1e9008e09e788cb4112f6
pial-nf <personalemail2106263@gmail.com>
2025-03-05 19:04:20 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,19 +5,19 @@ module.exports = {
   bot: {
     info: {
       prefix: '?',
-      token: 'MTI0MjQ2MDMzMzAyNTc4NzkyNg.Gz5-zb.2S5q0vTByM3Y3BJKkDlLgWUl7hSYr6BMsoricY',
-      invLink: 'https://discord.com/api/oauth2/authorize?client_id=1242460333025787926&permissions=8&scope=bot%20applications.commands',
-      privacy: 'https://discord.gg/teamkronix',
-      terms: 'https://discord.gg/teamkronix',
+      token: 'MTMzNDIxMDQ5NTE2NjU0NTk1MA.GboJLz.P1rmJQ4uZn-D5FuTkRKjSqqcOSudVflEYA1iKw',
+      invLink: 'https://discord.com/oauth2/authorize?client_id=1334210495166545950&permissions=8&integration_type=0&scope=bot',
+      privacy: 'https://discord.gg/YAqrQKS5qu',
+      terms: 'https://discord.gg/YAqrQKS5qu',
     },
     presence: {
       name: status,
       type: 'Listening',
-      url: 'https://discord.gg/teamkronix'
+      url: 'https://discord.gg/YAqrQKS5qu'
     },
     credits: {
-      developerId: '747321055319949312',
-      supportServer: 'https://discord.gg/teamkronix'
+      developerId: '713758139425751150',
+      supportServer: 'https://discord.gg/YAqrQKS5qu'
     },
   }
 }
